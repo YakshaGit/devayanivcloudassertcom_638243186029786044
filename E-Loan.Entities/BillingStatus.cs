6a90f7a73bef72f16b30fed4cc1ba247c6a710b3
muttanagouda@techademy.net <gituser@gmail.com>
2023-07-07 09:33:06 +0000

    c1

--- E-Loan.Entities/BillingStatus.cs
+++ E-Loan.Entities/BillingStatus.cs
@@ -10,3 +10,13 @@ namespace E_Loan.Entities
         Not_Salaried_Person = 2
     }
 }
+namespace E_Loan.Entities
+{
+    public enum BillingStatus
+    {
+        [Display(Name = "Salaried Persond")]
+        Salaried_Person = 1,
+        [Display(Name = "Not Salaried Persond")]
+        Not_Salaried_Person = 2
+    }
+}
